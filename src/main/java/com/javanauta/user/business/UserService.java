b43f1b1b47f8e22fa222c009630e42040e5e4a76
Enzo Bom <enzobom@gmail.com>
2025-12-12 17:05:56 -0300

    Final adjustments to get save method fully working

--- src/main/java/com/javanauta/user/business/UserService.java
+++ src/main/java/com/javanauta/user/business/UserService.java
@@ -3,18 +3,35 @@ package com.javanauta.user.business;
 import com.javanauta.user.business.converter.UserConverter;
 import com.javanauta.user.business.dto.UserDTO;
 import com.javanauta.user.infrastructure.entity.User;
+import com.javanauta.user.infrastructure.exceptions.ConflictException;
 import com.javanauta.user.infrastructure.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
     private final UserRepository userRepository;
     private final UserConverter userConverter;
+    private final PasswordEncoder passwordEncoder;
 
     public UserDTO saveUser(UserDTO userDTO) {
+        validateEmailNotExists(userDTO.getEmail());
+        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+
         User user = userConverter.toUser(userDTO);
         return userConverter.toUserDTO(userRepository.save(user));
+    }
+
+    public void validateEmailNotExists(String email) {
+        if (emailExists(email)) {
+            throw new ConflictException(String.format("Email '%s' already exists.", email));
+        }
+    }
+
+    public boolean emailExists(String email) {
+        return userRepository.existsByEmail(email);
     }
 }
